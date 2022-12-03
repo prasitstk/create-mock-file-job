1676bf6607ee80d211eceb71ccb8be22e7a09b7c
Prasit Sutthikamolsakul <51251028+prasitstk@users.noreply.github.com>
2022-12-03 08:37:50 +0700

    Create run-ssm-cmd-to-create-mock-file.sh

---
+++ run-ssm-cmd-to-create-mock-file.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# NOTE: Also required to set the following environment variables in addition to what we use below:
+# - AWS_ACCESS_KEY_ID=<AWS_ACCESS_KEY_ID>
+# - AWS_SECRET_ACCESS_KEY=<AWS_SECRET_ACCESS_KEY>
+# - AWS_REGION=<AWS_REGION>
+
+# Create /home/ec2-user/mainte/ikemen-ouji-api/db_mainte.json
+echo $(date -u '+%Y-%m-%d %H:%M:%S') - Start send-command to create a mock file...
+aws ssm send-command \
+    --document-name "create-mock-file" \
+    --parameters "mockContent='Mock Content'" \
+    --targets Key=resource-groups:Name,Values="$RESOURCE_GRP_NAME" \
+    --document-version '$DEFAULT'
+echo $(date -u '+%Y-%m-%d %H:%M:%S') - Finish send-command to create a mock file.
